5678cf68dc765a88a40d095203a535d1ae410595
wangsy <wangsy@zaozuo.com>
2017-05-26 01:16:47 +0800

    x

--- src/main/webapp/static/js/controllers.js
+++ src/main/webapp/static/js/controllers.js
@@ -62,7 +62,7 @@ angular.module('springPortfolio.controllers', ['ui.bootstrap'])
             tradeService.disconnect();
         };
 
-        tradeService.connect("/spring-websocket/portfolio")
+        tradeService.connect("/portfolio")
             .then(function (username) {
                     $scope.username = username;
                     pushNotification("Trade results take a 2-3 second simulated delay. Notifications will appear.");
